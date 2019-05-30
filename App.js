577e1c29cf41640b3140ffb91ea2ee3bb1438acb
Dan Halperin <dee.aye.en@gmail.com>
2019-05-30 10:41:24 +0300

    trying out mongo stitch

--- App.js
+++ App.js
@@ -1,4 +1,104 @@
-import React from 'react';
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  Stitch,
+  AnonymousCredential,
+  RemoteMongoClient
+} from "mongodb-stitch-react-native-sdk";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUserId: undefined,
+      client: undefined
+    };
+    this._loadClient = this._loadClient.bind(this);
+    this._onPressLogin = this._onPressLogin.bind(this);
+    this._onPressLogout = this._onPressLogout.bind(this);
+  }
+
+  componentDidMount() {
+    this._loadClient();
+  }
+
+  render() {
+    let loginStatus = "Currently logged out.";
+
+    if (this.state.currentUserId) {
+      loginStatus = `Currently logged in as ${this.state.currentUserId}!`;
+    }
+
+    loginButton = <Button onPress={this._onPressLogin} title="Login" />;
+
+    logoutButton = <Button onPress={this._onPressLogout} title="Logout" />;
+
+    return (
+      <View style={styles.container}>
+        <Text> {loginStatus} </Text>
+        {this.state.currentUserId !== undefined ? logoutButton : loginButton}
+      </View>
+    );
+  }
+
+  _loadClient() {
+    Stitch.initializeDefaultAppClient("taskmanager-hyxpx").then(client => {
+      this.setState({ client });
+
+      if (client.auth.isLoggedIn) {
+        this.setState({ currentUserId: client.auth.user.id });
+        const stitchAppClient = Stitch.defaultAppClient;
+        const mongoClient = stitchAppClient.getServiceClient(
+          RemoteMongoClient.factory,
+          "mongodb-atlas"
+        );
+        const db = mongoClient.db("taskmanager");
+        const tasks = db.collection("tasks");
+        tasks
+          .find({}, { limit: 10 })
+          .toArray()
+          .then(results => console.warn("Results:", results));
+      }
+    });
+  }
+
+  _onPressLogin() {
+    this.state.client.auth
+      .loginWithCredential(new AnonymousCredential())
+      .then(user => {
+        console.log(`Successfully logged in as user ${user.id}`);
+        this.setState({ currentUserId: user.id });
+      })
+      .catch(err => {
+        console.log(`Failed to log in anonymously: ${err}`);
+        this.setState({ currentUserId: undefined });
+      });
+  }
+
+  _onPressLogout() {
+    this.state.client.auth
+      .logout()
+      .then(user => {
+        console.log(`Successfully logged out`);
+        this.setState({ currentUserId: undefined });
+      })
+      .catch(err => {
+        console.log(`Failed to log out: ${err}`);
+        this.setState({ currentUserId: undefined });
+      });
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+/*import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
@@ -59,4 +159,4 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-});
+});*/
